8a4d4ed3ddee756814ffbbf9d99e972eeab0e173
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2024-01-15 23:35:24 -0500

    Move getImagesInPath function to main, change to using async/await functions, use preload file instead of ipc.send/on

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 const { app, BrowserWindow, screen, Menu, ipcMain, dialog } = require("electron");
 
 const createWindow = () => {
@@ -17,9 +18,10 @@ const createWindow = () => {
 			width: 1300,
 			height: 800,
 			webPreferences: {
-				nodeIntegration: true,
-				contextIsolation: false
-			}
+				nodeIntegration: false,
+				contextIsolation: true,
+				preload: path.join(__dirname, "preload.js"),
+			},
 		});
 	} else {
 		// Create window on default display
@@ -27,9 +29,10 @@ const createWindow = () => {
 			width: 1300,
 			height: 800,
 			webPreferences: {
-				nodeIntegration: true,
-				contextIsolation: false
-			}
+				nodeIntegration: false,
+				contextIsolation: true,
+				preload: path.join(__dirname, "preload.js"),
+			},
 		});
 	}
 	win.loadFile(path.join(__dirname, "./renderer/index.html"));
@@ -71,15 +74,32 @@ app.on("window-all-closed", () => {
 	}
 });
 
+// Get each image in the path
+function getImagesInPath(path) {
+	return new Promise((resolve, reject) => {
+		fs.readdir(path, (err, files) => {
+			if (err) {
+				console.error("An error occurred:", err);
+				reject(err);
+			} else {
+				const imageExtensions = [".jpeg", ".jpg", ".png", ".svg", ".tiff", ".raw", ".rw2"];
+				const imageFiles = files.filter(filename => {
+					const extension = filename.slice(filename.lastIndexOf("."));
+					return imageExtensions.includes(extension);
+				});
+				resolve(imageFiles);
+			}	
+		});
+	});
+}
+
 // Prompt for a directory when clicking the button
-ipcMain.on("open-folder-dialog", (event) => {
-	dialog.showOpenDialog({
-		properties: ["openDirectory"]
-	}).then(result => {
-		if (!result.canceled) {
-			event.sender.send("selected-directory", result.filePaths[0]);
-		}
-	}).catch(err => {
-		console.log(err);
-	})
+ipcMain.handle("open-folder-dialog", async (event) => {
+	const folder = await dialog.showOpenDialog({
+		properties: ["openDirectory"],
+	});
+	let path = folder.filePaths[0];
+	let listOfImages = await getImagesInPath(path);
+	return listOfImages;
+	// filter using function in renderer to get a new array of images to return to renderer
 });
