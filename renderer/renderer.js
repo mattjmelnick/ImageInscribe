54cfa146afaf4b69a5eeb465cadcc8c18f0a6baf
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2024-01-18 22:54:54 -0500

    Display path output and number of images

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,15 +1,16 @@
 let getFolderButton = document.querySelector(".get-folder-button");
 let pathOutput = document.querySelector(".path-output");
-let selectedDirectory;
+let numberOfImages = document.querySelector(".number-of-images");
 
 // Send choice to main process to get path
 getFolderButton.addEventListener("click", async () => {
 	const listOfImages = await window.api.selectFolder();
 	let folderPath = listOfImages.shift();
-	console.log(folderPath);
+	folderPath = folderPath.replaceAll("/", "/<wbr>");
 	pathOutput.innerHTML = "";
-	pathOutput.innerHTML = folderPath + "<br>" + listOfImages.length + "<br>";
-	for (let i = 1; i < listOfImages.length; i++) {
-		pathOutput.innerHTML = pathOutput.innerHTML + listOfImages[i] + "<br>";
-	}
+	pathOutput.innerHTML = "<span>" + folderPath + "<span>";
+	numberOfImages.textContent = listOfImages.length + " Images";
+	// for (let i = 1; i < listOfImages.length; i++) {
+	// 	pathOutput.innerHTML = pathOutput.innerHTML + listOfImages[i] + "<br>";
+	// }
 });
