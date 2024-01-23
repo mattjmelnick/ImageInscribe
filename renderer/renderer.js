2a47fc1e6ab2956973feeb18dc25bc31315623de
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2024-01-23 00:10:13 -0500

    Refactor code to add a create textbox wrapper function in the image file div class

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -2,6 +2,7 @@ let getFolderButton = document.querySelector(".get-folder-button");
 let pathOutput = document.querySelector(".path-output");
 let numberOfImages = document.querySelector(".number-of-images");
 let imageFileSection = document.querySelector(".image-file-section");
+let imageArray = [];
 
 // Send choice to main process to get path
 getFolderButton.addEventListener("click", async () => {
@@ -22,9 +23,76 @@ getFolderButton.addEventListener("click", async () => {
 	numberOfImages.textContent = listOfImages.length + " Images";
 	// Create image file sections for each element
 	for (let i = 0; i < listOfImages.length; i++) {
+		let imageFileDivObj = new imageFileDiv(listOfImages[i], i);
+		imageArray.push(imageFileDivObj);
+		// Create image file divs
 		let imageFile = document.createElement("div");
 		imageFile.classList.toggle("image-file");
-		imageFile.textContent = listOfImages[i];
 		imageFileSection.appendChild(imageFile);
+		imageFile.textContent = listOfImages[i];
+		
+		imageFileDivObj.createTextBoxWrapper();
+		
+		imageFile.addEventListener("click", () => {
+			let textBoxes = Array.from(document.querySelectorAll(".textbox-wrapper"));
+			textBoxes.forEach(box => {
+				box.style.display = "none";
+			});
+			imageFileDivObj.showTextbox();
+		});
 	}
 });
+
+class imageFileDiv {
+	constructor(name, index) {
+		this.name = name;
+		this.index = index;
+		this.textBoxWrapper = null;
+	}
+
+	createTextBoxWrapper() {
+		this.textBoxWrapper = document.createElement("div");
+		let upperTextBoxArea = document.createElement("div");
+		let textBoxTitle = document.createElement("div");
+		let closeButton = document.createElement("button");
+		let lowerTextBoxArea = document.createElement("div");
+		let textBox = document.createElement("textarea");
+		let buttonArea = document.createElement("div");
+		let upButton = document.createElement("button");
+		let downButton = document.createElement("button");
+
+		this.textBoxWrapper.classList.toggle("textbox-wrapper");
+		upperTextBoxArea.classList.toggle("upper-textbox-area");
+		textBoxTitle.classList.toggle("textbox-title");
+		closeButton.classList.toggle("close-button");
+		lowerTextBoxArea.classList.toggle("lower-textbox-area");
+		textBox.classList.toggle("textbox");
+		buttonArea.classList.toggle("button-area");
+		upButton.classList.toggle("up-button");
+		downButton.classList.toggle("down-button");
+
+		imageFileSection.appendChild(this.textBoxWrapper);
+		this.textBoxWrapper.appendChild(upperTextBoxArea);
+		this.textBoxWrapper.appendChild(lowerTextBoxArea);
+		upperTextBoxArea.appendChild(textBoxTitle);
+		upperTextBoxArea.appendChild(closeButton);
+		lowerTextBoxArea.appendChild(textBox);
+		lowerTextBoxArea.appendChild(buttonArea);
+		buttonArea.appendChild(upButton);
+		buttonArea.appendChild(downButton);
+
+		textBox.rows = "18";
+		textBox.cols = "25";
+	}
+
+	showTextbox() {
+		if (this.textBoxWrapper) {
+			this.textBoxWrapper.style.display = "inline-block";
+			let textBoxTitle = this.textBoxWrapper.querySelector(".textbox-title");
+			if (textBoxTitle) {
+				textBoxTitle.textContent = "";
+				textBoxTitle.textContent = this.name;
+			}
+		}
+	}
+}
